0c800b6dbfbdd13f7dd317cabc4aa476e9716eea
PC_13 <PC_13@DESKTOP-TN3LQIP>
2018-08-07 08:23:36 -0500

    Se modifico mensaje en main

--- Areas/src/mx/edu/itlapiedad/Areas.java
+++ Areas/src/mx/edu/itlapiedad/Areas.java
@@ -5,7 +5,7 @@ public class Areas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Triangulo t = new Triangulo(10, 20);
-		System.out.println(t.getArea());
+		System.out.println("El area del triangulo es: " + t.getArea());
 		
 		
 	}
