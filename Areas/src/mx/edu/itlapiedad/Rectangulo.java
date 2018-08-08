3f69eef5ce0249e31a58982c4b5461976f319b77
PC_12 <PC_12@192.168.1.69>
2018-08-08 10:34:35 -0500

    Se agrego la clase rectangulo

---
+++ Areas/src/mx/edu/itlapiedad/Rectangulo.java
@@ -0,0 +1,35 @@
+package mx.edu.itlapiedad;
+
+public class Rectangulo {
+
+	private int base, altura;
+
+	public Rectangulo(int base, int altura) {
+		super();
+		this.base = base;
+		this.altura = altura;
+	}
+
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	
+	public float getArea() {
+		
+		return base * altura;
+		
+	}
+	
+}
