166476f28a58628c0b7d442fad4a6cc1a189041a
PC_12 <PC_12@192.168.1.69>
2018-08-08 10:27:23 -0500

    2a. Modificación

--- Areas/src/mx/edu/itlapiedad/Areas.java
+++ Areas/src/mx/edu/itlapiedad/Areas.java
@@ -3,8 +3,8 @@ package mx.edu.itlapiedad;
 public class Areas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub  Modificado por colaborador
-		Triangulo t = new Triangulo(5, 3);
+		// TODO Auto-generated method stub  Modificado por colaborador 2
+		Triangulo t = new Triangulo(12, 13);
 		System.out.println("El area del triangulo es: " + t.getArea());
 		
 		
