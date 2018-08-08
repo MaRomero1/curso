6a3dee9984df3ac5217bace01c32379642101644
PC_13 <PC_13@192.168.1.72>
2018-08-08 07:56:24 -0500

    Se mando llamar la clase

--- Areas/src/mx/edu/itlapiedad/Areas.java
+++ Areas/src/mx/edu/itlapiedad/Areas.java
@@ -5,7 +5,9 @@ public class Areas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub  Modificado por colaborador 2
 		Triangulo t = new Triangulo(12, 13);
+		Rectangulo r = new Rectangulo (5, 6);
 		System.out.println("El area del triangulo es: " + t.getArea());
+		System.out.println("El area del rectangulo es: " + r.getArea());
 		
 		
 	}
