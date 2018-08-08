1585285a292f0599df66bbeb3ab9005c28c47f00
PC_12 <PC_12@192.168.1.69>
2018-08-08 10:18:06 -0500

    Cambios hechos al archivo de estela

--- Areas/src/mx/edu/itlapiedad/Areas.java
+++ Areas/src/mx/edu/itlapiedad/Areas.java
@@ -3,8 +3,8 @@ package mx.edu.itlapiedad;
 public class Areas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Triangulo t = new Triangulo(10, 20);
+		// TODO Auto-generated method stub  Modificado por colaborador
+		Triangulo t = new Triangulo(5, 3);
 		System.out.println("El area del triangulo es: " + t.getArea());
 		
 		
